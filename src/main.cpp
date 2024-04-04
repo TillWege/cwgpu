73c661f0cda4389155f462da3b4903a16e7c9037
TillWege <tillwege@gmail.com>
2024-04-04 21:59:44 +0200

    add wgpu

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+#include <webgpu/webgpu.h>
 
-int main()
-{
-	std::cout << "Hello, World!" << std::endl;
+int main (int, char**) {
+	WGPUInstanceDescriptor desc = {};
+	desc.nextInChain = nullptr;
+
+	WGPUInstance instance = wgpuCreateInstance(&desc);
+
+	if (!instance) {
+		std::cerr << "Could not initialize WebGPU!" << std::endl;
+		return 1;
+	}
+
+	std::cout << "WGPU instance: " << instance << std::endl;
+
+	if (!glfwInit()) {
+		std::cerr << "Could not initialize GLFW!" << std::endl;
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "CWGPU", NULL, NULL);
+	if (!window) {
+		std::cerr << "Could not open window!" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	wgpuInstanceRelease(instance);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
