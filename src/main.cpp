8e06f53abdec442b833eec03f4f0c5014e05739d
TillWege <tillwege@gmail.com>
2024-04-05 17:16:10 +0200

    get surface + adapter extensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include "wgpu_utils.h"
+#include <glfw3webgpu.h>
+#include <vector>
 
 int main (int, char**) {
 	WGPUInstanceDescriptor desc = {};
@@ -20,24 +22,48 @@ int main (int, char**) {
 		return 1;
 	}
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "CWGPU", NULL, NULL);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "CWGPU", nullptr, nullptr);
 	if (!window) {
 		std::cerr << "Could not open window!" << std::endl;
 		glfwTerminate();
 		return 1;
 	}
 
-	std::cout << "Requesting adapter..." << std::endl;
+	std::cout << "Requesting surface..." << std::endl;
+	WGPUSurface surface = glfwGetWGPUSurface(instance, window);
+	std::cout << "Got surface" << surface << std::endl;
 
+	std::cout << "Requesting adapter..." << std::endl;
 	WGPURequestAdapterOptions adapterOpts = {};
+	adapterOpts.nextInChain = nullptr;
+	adapterOpts.compatibleSurface = surface;
 	WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
 
 	std::cout << "Got adapter: " << adapter << std::endl;
 
+	// Check features of adapter
+
+	std::vector<WGPUFeatureName> features;
+
+	// Call the function a first time with a null return address, just to get the entry count.
+	size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+	// Allocate memory (could be a new, or a malloc() if this were a C program)
+	features.resize(featureCount);
+
+	// Call the function a second time, with a non-null return address
+	wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+	std::cout << "Adapter features:" << std::endl;
+	for (auto f : features) {
+		std::cout << " - " << f << std::endl;
+	}
+
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
-	
+
 	wgpuAdapterRelease(adapter);
 	wgpuInstanceRelease(instance);
 
